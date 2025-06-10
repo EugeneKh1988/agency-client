a89527a1c72085e7234f7f668608cf0db4a8553b
Evgeniy <khiznyak.1988@gmail.com>
2025-06-10 09:49:37 +0300

    added remote pattern for images

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,11 @@ const nextConfig: NextConfig = {
         port: "8000",
       },
       {
+        protocol: "http",
+        hostname: "192.168.0.172",
+        port: "8000",
+      },
+      {
         protocol: "https",
         hostname: "green-shop-og6h.onrender.com",
         port: "80",
